dabe285e6c46be813dc2110f62426f4ae24f6e78
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-04-26 20:52:07 +0530

    added api home route

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/api/user", (req, res) => {
   return res.json({ id: 1, name: "Nikhil", email: "n@test.com" });
 });
 
+app.get("/api/home", (req, res) => {
+  return res.send("Welcome to API Home, try running api routes");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("App open at port: ", PORT);
