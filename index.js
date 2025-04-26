328e3a0ae8d40b97d8e7dad8369a5bd37bb09825
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-04-26 20:22:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  return res.send("Health OK!");
+});
+
+app.get("/api/user", (req, res) => {
+  return res.json({ id: 1, name: "Nikhil", email: "n@test.com" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("App open at port: ", PORT);
+});
